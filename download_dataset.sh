55e1be74d06760ce87b42d812a3fc12ddf0a0fc0
Tingkai Liu <40692282+TingkaiLiu@users.noreply.github.com>
2024-03-13 14:13:04 -0500

    Update download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -7,10 +7,11 @@ set -e
 
 # download dataset
 cd ~
-wget http://images.cocodataset.org/zips/train2017.zip
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+# wget http://images.cocodataset.org/zips/train2017.zip
+# wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+curl -L https://zenodo.org/record/6615455/files/PASS.0.tar --output PASS.0.tar
 
 # use 7zip to decompress with multi-thread (7zip should already be installed)
-7z x train2017.zip
-7z x annotations_trainval2017.zip
-
+# 7z x train2017.zip
+# 7z x annotations_trainval2017.zip
+tar -xf PASS.0.tar
